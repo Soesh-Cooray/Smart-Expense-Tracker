76239009667549abfcdb000e98aee89f4a6f5c5f
Soesh Cooray <soeshcooray@gmail.com>
2026-02-23 11:30:25 +0530

    initial commit

---
+++ src/main/java/org/example/smart_expense_tracker/SmartExpenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.example.smart_expense_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartExpenseTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartExpenseTrackerApplication.class, args);
+    }
+
+}
