fac1a8265c239acd0241f8c8dd0ad442582f2a4c
Soesh Cooray <soeshcooray@gmail.com>
2026-02-25 18:44:07 +0530

    added sidebar component

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Index from './Index.jsx'
+import Login from './Login.jsx'
+import Signup from './Signup.jsx'
+import Dashboard from './Dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Index />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
